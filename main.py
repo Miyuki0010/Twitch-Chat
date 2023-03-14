2dd61fff727014ffd5d7f9aca2f2ef017d64c006
Alyss <alcatratzhd@gmail.com>
2023-03-14 22:32:07 +0100

    updated the bot itself

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import random
 import asyncio
-import twitchio
 import json
+import random
+
+import twitchio
 
 # Load bot config from json file
 with open('config.json', 'r') as f:
     bot_config = json.load(f)
 
-# Extreact config params
+# Extract config params
 BOT_USERNAME = bot_config['bot_username']
 OAUTH_TOKEN = bot_config['oauth_token']
 CHANNEL_NAME = bot_config['channel_name']
@@ -15,53 +16,36 @@ BOT_PREFIX = bot_config['bot_prefix']
 timer_minutes = bot_config['initial_timer']['minutes']
 timer_seconds = bot_config['initial_timer']['seconds']
 
-# intialziaton bot
+# Initialization bot
 bot = twitchio.Client(
     token=OAUTH_TOKEN
 )
 
-# join the initial channel
+# Join the initial channel
 bot.loop.run_until_complete(
     bot.join_channels(
         [CHANNEL_NAME]
     )
 )
 
-
-# intialziaton of counter and timer
+# Initialization of counter and timer
 counter = 0
 timer = None
-
 
 # Initialization of followers + influence on timer
 followers = set()
 follower_influence = 0
 
 
-# Get inital set of followers
-async def get_inital_followers():
-    global followers, follower_influence
-
-    followers.response = await bot.get_chatters(CHANNEL_NAME)
-    initial_followers = followers.resonse.all
-
-    # Assign initial influence to followers
-    follower_influence = len(initial_followers) * 5
-
-    # Add followers to set
-    for follower in initial_followers:
-        followers.add(follower)
-
-
 # Define func to update and start run_timer
 async def on_subscribe(subscriber, channel):
     global counter, timer, follower_influence
 
-    # Increase counter and add subscriber to followers set
+    # Increase counter and add subs to followers set
     counter += 1
     followers.add(subscriber.name)
 
-    # if first sub, add their influence to timer
+    # If first sub, add their influence to timer
     if counter == 1:
         follower_influence += 5
 
@@ -69,28 +53,33 @@ async def on_subscribe(subscriber, channel):
     minutes = random.randint(20, 45) + follower_influence
     seconds = minutes * 60
 
-   # Start timer, if it isn't already running
+    # Start timer, if it isn't already running
     if timer is None:
-       timer = asyncio.create_task(run_timer())
+        timer = asyncio.create_task(run_timer())
+    else:
+        # If timer has already finished, set it to None
+        if timer.done():
+            timer = None
 
 
-# Define func to run timer print remainding time
+# Define func to run timer and print remaining time
 async def run_timer():
-    global counter, timer, marathon_running
+    global counter
 
     while counter > 0:
         minutes = random.randint(20, 45) + follower_influence
         seconds = minutes * 60
 
-        while counter > 0 and  marathon_running:
+        while seconds > 0:
+            hours, seconds = divmod(seconds, 3600)
             minutes, seconds = divmod(seconds, 60)
-            hours, minutes = divmod(minutes, 60)
+            remaining_time = seconds + minutes * 60 + hours * 3600
             print(f"{hours:02d}:{minutes:02d}")
             await asyncio.sleep(1)
 
         counter -= 1
 
-    timer = None
+    return remaining_time
 
 
 # Register on_sub as event handler
@@ -98,41 +87,31 @@ async def event_subscribe(subscriber, channel):
     await on_subscribe(subscriber, channel)
 
 
+# Process messages
 async def event_message(ctx):
-    global follower_influence, marathon_running, timer
+    global follower_influence
 
     # If message is from bot or does not have the prefix, it gets ignored
-    if ctx.author.name.lower() == BOT_USERNAME.lower() or not ctx.content.startswith(BOT_PREFIX) :
+    if ctx.author.name.lower() == BOT_USERNAME.lower() or not ctx.content.startswith(BOT_PREFIX):
         return
 
-    # Parse command 
+    # Parse command
     command, *args = ctx.content.split(' ')
 
-
-    # Only the channel owner or moderators can pause/resume the marathon
-    if ctx.author.name.lower() == CHANNEL_NAME.lower() and (ctx.author.is_mod or ctx.author.is_owner):
-
-        # Pause the marathon
-        if command == f"{BOT_PREFIX}pause":
-            marathon_running = False
-            await ctx.channel.send("The marathon has been paused.")
-
-        # Resume the marathon
-        elif command == f"{BOT_PREFIX}resume":
-            marathon_running = True
-            timer = asyncio.create_task(run_timer())
-            await ctx.channel.send("The marathon has been resumed.")
-
-
-    # If command is !remaining. print time
+    # If command is !remaining, print time
     if command == f"{BOT_PREFIX}remaining":
-        minutes, seconds = divmod(timer._coro.cr_frame.f_locals['seconds'],60)
-        await ctx.channel.send(f"Remaining time: {minutes:02d}:{seconds:02d}")
+        if timer is None:
+            await ctx.channel.send("There is no marathon currently running.")
+        else:
+            remaining_time = await run_timer() if timer is not None else 0
+            hours, remaining_time = divmod(remaining_time, 3600)
+            minutes, seconds = divmod(remaining_time, 60)
+            await ctx.channel.send(f"Remaining time: {hours:02d}:{minutes:02d}")
     else:
-        await ctx.channel.send("There is no marathon curently running.")
+        await ctx.channel.send("There is no marathon currently running.")
 
-    # Add cooldown to the command
-    await bot._ws.send_privmsg(ctx.channel.name, f".cooldown {BOT_USERNAME} 5")
+        # Add cooldown to the command
+        await ctx.send(f".cooldown {BOT_USERNAME} 5")
 
 
 # Start the bot
