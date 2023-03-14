834d53c3bfcef47bffc655d168f0078db02055cd
Alyss <alcatratzhd@gmail.com>
2023-03-14 22:52:04 +0100

    updated everything, added 2 new commands

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ bot.loop.run_until_complete(
 # Initialization of counter and timer
 counter = 0
 timer = None
+is_paused = False
 
 # Initialization of followers + influence on timer
 followers = set()
@@ -64,22 +65,38 @@ async def on_subscribe(subscriber, channel):
 
 # Define func to run timer and print remaining time
 async def run_timer():
-    global counter
+    global counter, timer, is_paused
 
     while counter > 0:
-        minutes = random.randint(20, 45) + follower_influence
-        seconds = minutes * 60
+        if not is_paused:
+            minutes = random.randint(20, 45) + follower_influence
+            seconds = minutes * 60
 
-        while seconds > 0:
-            hours, seconds = divmod(seconds, 3600)
-            minutes, seconds = divmod(seconds, 60)
-            remaining_time = seconds + minutes * 60 + hours * 3600
-            print(f"{hours:02d}:{minutes:02d}")
-            await asyncio.sleep(1)
+            while seconds > 0 and not is_paused:
+                hours, seconds = divmod(seconds, 3600)
+                minutes, seconds = divmod(seconds, 60)
+                remaining_time = seconds + minutes * 60 + hours * 3600
+                print(f"{hours:02d}:{minutes:02d}")
+                await asyncio.sleep(1)
 
-        counter -= 1
+            counter -= 1
 
-    return remaining_time
+    timer = None
+
+
+# Define func to toggle the timer
+async def pause_resume_timer(ctx):
+    global is_paused
+
+    # If the command is used by the channel owner or a mod, toggle the timer
+    if ctx.author.is_mod or ctx.author.name.lower() == CHANNEL_NAME.lower():
+        is_paused = not is_paused
+
+        # Send message to the chat
+        if is_paused:
+            await ctx.channel.send("Timer is now paused.")
+        else:
+            await ctx.channel.send("Timer has resumed.")
 
 
 # Register on_sub as event handler
@@ -89,7 +106,7 @@ async def event_subscribe(subscriber, channel):
 
 # Process messages
 async def event_message(ctx):
-    global follower_influence
+    global follower_influence, timer
 
     # If message is from bot or does not have the prefix, it gets ignored
     if ctx.author.name.lower() == BOT_USERNAME.lower() or not ctx.content.startswith(BOT_PREFIX):
@@ -107,11 +124,29 @@ async def event_message(ctx):
             hours, remaining_time = divmod(remaining_time, 3600)
             minutes, seconds = divmod(remaining_time, 60)
             await ctx.channel.send(f"Remaining time: {hours:02d}:{minutes:02d}")
+    elif command == f"{BOT_PREFIX}pause" and (await ctx.get_permissions(ctx.author)).moderator or await is_owner(ctx):
+        if timer is None:
+            await ctx.channel.send("There is no marathon currently running.")
+        else:
+            timer.cancel()
+            timer = None
+            await ctx.channel.send("The marathon has been paused.")
+    elif command == f"{BOT_PREFIX}resume" and (await ctx.get_permissions(ctx.author)).moderator or await is_owner(ctx):
+        if timer is None:
+            await ctx.channel.send("There is no marathon currently running.")
+        else:
+            timer = asyncio.create_task(run_timer())
+            await ctx.channel.send("The marathon has been resumed.")
     else:
-        await ctx.channel.send("There is no marathon currently running.")
+        await ctx.channel.send("Invalid command.")
 
         # Add cooldown to the command
         await ctx.send(f".cooldown {BOT_USERNAME} 5")
+
+
+# Check if user is a mod or the channel owner
+async def is_owner(ctx):
+    return await ctx.get_permissions(ctx.author).owner
 
 
 # Start the bot
